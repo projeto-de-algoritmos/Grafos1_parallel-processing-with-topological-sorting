347c8a8fa11ecf196ec7843b4f21468f26b36c8d
Felipe Borges <bumbleblo2013@gmail.com>
2021-02-12 11:57:40 -0300

    Finishing cycle match function and adding some tests
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Co-authored-by: Victor Jorge <victor.eng.unb@gmail.com>

--- lista1/question_2.cpp
+++ lista1/question_2.cpp
@@ -3,7 +3,37 @@
 using namespace std;
 
 vector<vector<int>> graph;
+vector<int> visited;
+int start = -1;
 
+bool detected_cycle(int prev, int node){
+
+    visited[node] = 2;
+
+    bool edge_have_cycle = false;
+
+    for(auto edge : graph[node]){
+
+        if(prev != edge) {
+            if(visited[edge] == -1){
+                edge_have_cycle = edge_have_cycle or detected_cycle(node, edge);
+            }
+
+            if(visited[edge] == 2){
+                start = edge;
+                edge_have_cycle = true;
+            }
+        }
+    }
+
+    visited[node] = 1;
+
+    // if some edge is in the cycle and i'am not the start of the cycle propagate that
+    if(edge_have_cycle)
+        cout << "-> "<<  node << endl;
+
+    return edge_have_cycle and (start != node);
+}
 
 void show_graph(){
     for(int i = 0; i<graph.size(); ++i){
@@ -27,7 +57,8 @@ int main(){
     cout << "Numero de ligações" << endl;
     cin >> m;
 
-    graph = vector<vector<int>>(n);
+    graph.resize(n);
+    visited = vector<int>(n, -1);
 
     int a, b;
     for(int i = 0; i<m; ++i){
@@ -40,6 +71,9 @@ int main(){
     }
 
     show_graph();
+
+    cout << "Nós que fazem parte do ciclo" << endl;
+    detected_cycle(-1, 0);
 
     return 0;
 }

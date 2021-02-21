fd90036805fe49b02701ea04158e10b8f8082440
Felipe Borges <bumbleblo2013@gmail.com>
2021-02-21 15:23:46 -0300

    Adding demo
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>

---
+++ demo.py
@@ -0,0 +1,93 @@
+import time
+from collections.abc import Callable
+from top_processing import lazy, relational_graph, count_graph, graph, Evaluate
+from top_processing.parallel import process
+from top_processing.topological import topological_sorting
+
+def delay(function: Callable, seconds:int = 1):
+    """
+    Waite few seconds before call a function
+    """
+
+    def decorated(*args, **kwargs):
+
+        time.sleep(seconds)
+
+        response = function(*args, **kwargs)
+
+        return response
+
+    return decorated
+
+@lazy
+@delay
+def add(x: int, y: int):
+    return x  + y
+
+@lazy
+@delay
+def mult(x: int, y: int):
+    return x*y
+
+@lazy
+@delay
+def attr_val(x: int):
+    return x
+
+
+def create_expression():
+
+    #define expression
+    arg1: Evaluate = attr_val(5)
+    arg2: Evaluate = attr_val(4)
+
+    add_1: Evaluate = add(arg1, arg2)
+
+    arg3: Evaluate = attr_val(10)
+    arg4: Evaluate = attr_val(4)
+
+    mult_1: Evaluate = mult(arg3, arg4)
+
+    expression: Evaluate = add(add_1, mult_1)
+
+    return expression
+
+def clear_graphs():
+
+    graph = {}
+    relational_graph = {}
+    count_graph = {}
+
+if __name__ == '__main__':
+
+    # SEQUENCIAL EXECUTION
+    print("Execution the expression sequentially")
+    start = time.time()
+
+    expression = create_expression()
+    response = expression()
+
+    end = time.time()
+
+    clear_graphs()
+
+    print(f"Result of expression: {response}")
+    print(f"Time of execution: {end - start:.2f}s")
+
+    expression = create_expression()
+
+    # PARALLEL EXECUTION
+    print("Execution the expression in parallel")
+
+    start = time.time()
+
+    # running topological sorting
+    t_sort = topological_sorting(relational_graph.copy(), count_graph.copy(), graph.copy())
+
+    response = process(t_sort, count_graph.copy(), relational_graph.copy())
+
+    end = time.time()
+
+    print(f"Result of expression: {response}")
+    print(f"Time of execution: {end - start:.2f}s")
+
